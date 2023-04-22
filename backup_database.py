ba7d0706a463a9d02111c5e71c44723ae17a769d
mitchellggreenhalgh <63256822+mitchellggreenhalgh@users.noreply.github.com>
2023-04-22 16:53:49 -0500

    Backup Database Class

--- backup_database.py
+++ backup_database.py
@@ -3,22 +3,22 @@ from datetime import date, datetime, timedelta
 from glob import glob
 from shutil import copyfile
 
-db_files = glob('backup_DBs/backup*.db')
+class db_backupper:
+    db_files =  glob('backup_DBs/backup*.db')
+    now = datetime.now()
+    timestamps = [datetime.fromtimestamp(os.path.getctime(db)) for db in db_files]
+    backup_period = timedelta(days = 30)
+    db_name = 'runningDB_master.db'
+    db_backup_name = "backup_DBs/backup_runningDB_" + str(date.today()) + '.db'
 
-time_since_backup = []
-for db in db_files:
-    time_since_backup.append(datetime.now() - datetime.fromtimestamp(os.path.getctime(db)))
-
-backup_period = timedelta(days=30)
-if not any(time < backup_period for time in time_since_backup):  # TODO override by removing 'not'
-    try:
-        db_name = 'runningDB_master.db'
-        db_backup_name = "backup_DBs/backup_runningDB_" + str(date.today()) + '.db'
-        copyfile(db_name, db_backup_name)
+    def _backup(self):
+        time_since_backup = [self.now - tmstmp for tmstmp in self.timestamps]
+        if not any(time < self.backup_period for time in time_since_backup):
+                copyfile(self.db_name, self.db_backup_name)
+                print('New backup created')
+        else:
+            raise Exception('Recent backup within last 30 days found.')
     
-    except:
-        pass
-    
-else:
-    raise Exception('Recent backup within last 30 days found.')
-    
+    def _backup_override(self):
+         copyfile(self.db_name, self.db_backup_name)
+         print('New backup created')
