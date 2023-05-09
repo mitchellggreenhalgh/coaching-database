98121437da4b9a7d3522cfe86bb9e40bcdcaa5e5
mitchellggreenhalgh <63256822+mitchellggreenhalgh@users.noreply.github.com>
2023-05-09 17:55:28 -0500

    fixed bug in time_math partial seconds

--- time_math.py
+++ time_math.py
@@ -2,12 +2,9 @@ class time_addition:
 
     def add_time(str_list:list or tuple):
         minutes = sum([int(time[0]) for time in str_list])
-        seconds = sum([int(sec[2:4]) for sec in str_list])
-        partial_seconds = sum([int(hundredth[-2:]) for hundredth in str_list])
+        seconds = sum([float(sec[2:]) for sec in str_list])
 
-        minutes += seconds // 60
+        minutes += int(seconds // 60)
         remaining_seconds = seconds % 60
-        seconds += partial_seconds // 100
-        remaining_partial_seconds = partial_seconds % 100
 
-        return f'{minutes}:{remaining_seconds:02}.{remaining_partial_seconds:02}'
+        return f'{minutes}:{remaining_seconds:02}'
